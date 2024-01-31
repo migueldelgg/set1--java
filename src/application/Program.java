dc7a3b6f3e49d3c8c93bf5b48faec571e5b9ab0b
4Miguel2 <delgdevoleper@gmail.com>
2024-01-31 13:45:21 -0300

    set: contagem de usúarios

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file full path: ");
+        String path = sc.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while(line != null) {
+
+                String[] fields = line.split(" ");
+                String userName = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+                set.add(new LogEntry(userName, moment));
+
+                line = br.readLine();
+                // set.add(new LogEntry(fields[0], Date.from(Instant.parse(fields[1]))));
+            }
+            System.out.println("Total users: " + set.size());
+
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            sc.close();
+        }
+    }
+
+
+}
